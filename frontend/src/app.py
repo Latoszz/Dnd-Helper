abff0eed8c4ae602185ea34422655c3a20110861
Latoszz <118390289+Latoszz@users.noreply.github.com>
2025-05-20 21:47:42 +0200

    Update app.py

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -2,16 +2,20 @@ import os
 import streamlit as st
 from dotenv import load_dotenv
 from config.config_manager import ConfigManager
+from services.service import BackendService
+
 
 config_manager = ConfigManager("config.yml")
-config = config_manager.get_config()
+config = config_manager.get_config().get('app',{})
+
+service = BackendService()
 
 class FrontendApp:
 
     def __init__(self):
         st.set_page_config(
-            page_title=config.get('app', {}).get('title'),
-            layout=config.get('app', {}).get('layout')
+            page_title=config.get('title'),
+            layout=config.get('layout')
         )
 
     def run(self):
