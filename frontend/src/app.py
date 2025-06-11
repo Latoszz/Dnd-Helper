1c10b1bcb4e82e9d12e6a26b4b001793e8b9f8e7
Latoszz <118390289+Latoszz@users.noreply.github.com>
2025-06-12 00:03:56 +0200

    Updated app.py

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -7,26 +7,21 @@ from services.frontend_service import FrontendService
 from components.sidebar import SidebarComponent
 from managers.session_state_manager import SessionStateManager
 
-
-
-
 class FrontendApp:
 
     def __init__(self):
         config_manager = ConfigManager("frontend/src/config/config.yml")
         self.config = config_manager.get_config().get('app')
         self.service = FrontendService(config_manager)
-
         self.sidebar = SidebarComponent(self.config)
-
-        self.session = SessionStateManager(self.config)
         st.set_page_config(
             page_title=self.config.get('title'),
             layout=self.config.get('layout')
         )
 
     def display_chat(self):
-        for message in self.session.get_messages():
+        messages = SessionStateManager.get_messages()
+        for message in messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
 
@@ -34,34 +29,33 @@ class FrontendApp:
         if prompt:
             with st.chat_message("user"):
                 st.markdown(prompt)
-            self.session.add_message("user",prompt)
-
+            SessionStateManager.add_message("user",prompt)
+            ai_model = SessionStateManager.get_ai_model()
+            temperature = SessionStateManager.get_temperature()
             try:
-                print({'question': prompt,
-                        'model': self.session.get_ai_model_name(),
-                        'provider': self.session.get_ai_model_provider(),
-                         'temperature': self.session.get_temperature()})
-                response_data = asyncio.run(
-                    self.service
-                    .post_data('chat', {
+                request_data = {
                         'question': prompt,
-                        'model': self.session.get_ai_model_name(),
-                        'provider': self.session.get_ai_model_provider(),
-                         'temperature': self.session.get_temperature()})
+                        'model': ai_model['model'],
+                        'provider': ai_model['provider'],
+                        'temperature': temperature}
+                print(f"POST: {request_data}")
+                response_data = asyncio.run(
+                    self.service.post_data('chat', request_data)
                 )
+
                 response = f"{response_data}"
             except Exception as e:
                 response = f"Error: {str(e)}"
 
             with st.chat_message("assistant"):
                 st.markdown(response)
-            self.session.add_message("assistant",response)
+            SessionStateManager.add_message("assistant",response)
 
 
     def run(self):
         st.title("DnD bot")
 
-        self.session.initialize()
+        SessionStateManager(self.config).initialize()
         self.sidebar.display()
         self.display_chat()
 
